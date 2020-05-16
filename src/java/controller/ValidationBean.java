56bde5d53e6533082597545c4d0283bfdd7c9cd2
Onur ASLAN <=theeaslann1@gmail.com>
2020-05-17 00:33:24 +0300

    validationBean class'i tamamlandı , validationBean (kitapCat->form)

---
+++ src/java/controller/ValidationBean.java
@@ -0,0 +1,27 @@
+
+package controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+
+
+@Named
+@SessionScoped
+public class ValidationBean implements Serializable {
+    
+    
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException{
+        String str = value.toString();
+        if(str.length()<3){
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "summary", "en az 3 karekter"));
+        }
+        if(str.length()>25){
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "summary", "en fazla 5"));
+        }
+    }
+}
