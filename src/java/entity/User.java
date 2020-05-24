0fcf392c414fcc5ae2d168ec39df408853a3c26a
Onur ASLAN <=theeaslann1@gmail.com>
2020-05-25 02:56:06 +0300

    Süper admin tarafından yetki vermek tablosu (user_yetki)  tamamlandi yanlızca admin tarafından tabloya (crud) işlemeri yapar ... veritabanında ilk degerler 0.daha sonra yetki vermek için deger 1 yapılamsı gerekir. Full yetki için : c=1 ; r=1 ; u=1 ; d=1 ;

--- src/java/entity/User.java
+++ src/java/entity/User.java
@@ -66,6 +66,16 @@ public class User implements Serializable {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+
     public String getUturu() {
         return uturu;
     }
